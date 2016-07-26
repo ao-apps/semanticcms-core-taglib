cd3cdb5bd37efd77389eb8fbada33fb508d578a3
Dan Armstrong <dan@aoindustries.com>
2016-07-26 21:36:15 +0000

    Splitting non-km parts into more general purpose taglib.

--- src/com/aoindustries/web/page/taglib/LinkTag.java
+++ src/com/aoindustries/web/page/taglib/LinkTag.java
@@ -22,10 +22,6 @@
  */
 package com.aoindustries.web.page.taglib;
 
-import com.aoindustries.encoding.MediaWriter;
-import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.encodeTextInXhtmlAttribute;
-import static com.aoindustries.encoding.TextInXhtmlEncoder.encodeTextInXhtml;
-import static com.aoindustries.encoding.TextInXhtmlEncoder.textInXhtmlEncoder;
 import com.aoindustries.io.NullWriter;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.io.buffer.SegmentedWriter;
@@ -33,26 +29,13 @@ import com.aoindustries.net.EmptyParameters;
 import com.aoindustries.net.HttpParameters;
 import com.aoindustries.net.HttpParametersMap;
 import com.aoindustries.net.MutableHttpParameters;
-import com.aoindustries.servlet.http.LastModifiedServlet;
 import com.aoindustries.servlet.jsp.LocalizedJspTagException;
 import static com.aoindustries.taglib.ApplicationResources.accessor;
 import com.aoindustries.taglib.ParamUtils;
 import com.aoindustries.taglib.ParamsAttribute;
-import com.aoindustries.taglib.UrlUtils;
-import com.aoindustries.web.page.Element;
-import com.aoindustries.web.page.Node;
-import com.aoindustries.web.page.Page;
-import com.aoindustries.web.page.PageRef;
 import com.aoindustries.web.page.servlet.CaptureLevel;
-import com.aoindustries.web.page.servlet.CapturePage;
-import com.aoindustries.web.page.servlet.CurrentNode;
-import com.aoindustries.web.page.servlet.CurrentPage;
-import com.aoindustries.web.page.servlet.LinkImpl;
-import com.aoindustries.web.page.servlet.PageIndex;
-import com.aoindustries.web.page.servlet.PageRefResolver;
+import com.aoindustries.web.page.servlet.impl.LinkImpl;
 import java.io.IOException;
-import java.net.URLEncoder;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -60,6 +43,7 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.SkipPageException;
 import javax.servlet.jsp.tagext.DynamicAttributes;
 import javax.servlet.jsp.tagext.JspFragment;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
@@ -132,184 +116,59 @@ public class LinkTag
 
 	@Override
 	public void doTag() throws JspException, IOException {
-		if(page==null && element==null && view==null) throw new JspTagException("If neither element nor view provided, then page is required.");
+		try {
+			final PageContext pageContext = (PageContext)getJspContext();
+			final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
 
-		final PageContext pageContext = (PageContext)getJspContext();
-		final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
-
-		// Get the current capture state
-		final CaptureLevel captureLevel = CaptureLevel.getCaptureLevel(request);
-		if(captureLevel.compareTo(CaptureLevel.META) >= 0) {
-			// Capture the body first for any nested parameter tags
-			BufferResult capturedBody;
-			if(captureLevel == CaptureLevel.BODY) {
-				JspFragment body = getJspBody();
-				if(body != null) {
-					SegmentedWriter captureOut = new SegmentedWriter();
-					try {
-						body.invoke(captureOut);
-					} finally {
-						captureOut.close();
+			// Get the current capture state
+			final CaptureLevel captureLevel = CaptureLevel.getCaptureLevel(request);
+			if(captureLevel.compareTo(CaptureLevel.META) >= 0) {
+				// Capture the body first for any nested parameter tags
+				BufferResult capturedBody;
+				if(captureLevel == CaptureLevel.BODY) {
+					JspFragment body = getJspBody();
+					if(body != null) {
+						SegmentedWriter captureOut = new SegmentedWriter();
+						try {
+							body.invoke(captureOut);
+						} finally {
+							captureOut.close();
+						}
+						capturedBody = captureOut.getResult().trim();
+					} else {
+						capturedBody = null;
 					}
-					capturedBody = captureOut.getResult().trim();
 				} else {
+					// Invoke body for any meta data, but discard any output
+					JspFragment body = getJspBody();
+					if(body != null) body.invoke(NullWriter.getInstance());
 					capturedBody = null;
 				}
-			} else {
-				// Invoke body for any meta data, but discard any output
-				JspFragment body = getJspBody();
-				if(body != null) body.invoke(NullWriter.getInstance());
-				capturedBody = null;
-			}
-			
-			final Node currentNode = CurrentNode.getCurrentNode(request);
-			final Page currentPage = CurrentPage.getCurrentPage(request);
-			
-			final ServletContext servletContext = pageContext.getServletContext();
-			// Use current page when page not set
-			PageRef targetPageRef;
-			if(page == null) {
-				if(this.book != null) throw new JspTagException("page must be provided when book is provided.");
-				if(currentPage == null) throw new JspTagException("<p:link> must be nested in <p:page> when page attribute not set.");
-				targetPageRef = currentPage.getPageRef();
-			} else {
-				try {
-					targetPageRef = PageRefResolver.getPageRef(servletContext, request, this.book, this.page);
-				} catch(ServletException e) {
-					throw new JspTagException(e);
-				}
-			}
-			// Add page links
-			if(currentNode != null) currentNode.addPageLink(targetPageRef);
-			if(captureLevel == CaptureLevel.BODY) {
-				final HttpServletResponse response = (HttpServletResponse)pageContext.getResponse();
-				final String responseEncoding = response.getCharacterEncoding();
-
-				// Capture the page
-				Page targetPage;
-				if(targetPageRef.getBook()==null) {
-					targetPage = null;
-				} else if(
-					// Short-cut for element already added above within current page
-					currentPage != null
-					&& targetPageRef.equals(currentPage.getPageRef())
-					&& (
-						element==null
-						|| currentPage.getElementsById().containsKey(element)
-					)
-				) {
-					targetPage = currentPage;
-				} else {
-					// Capture required, even if capturing self
-					try {
-						targetPage = CapturePage.capturePage(
-							servletContext,
-							request,
-							response,
-							targetPageRef,
-							element==null ? CaptureLevel.PAGE : CaptureLevel.META
-						);
-					} catch(ServletException e) {
-						throw new JspTagException(e);
-					}
-				}
-
-				// Find the element
-				Element targetElement;
-				if(element != null && targetPage != null) {
-					targetElement = targetPage.getElementsById().get(element);
-					if(targetElement == null) throw new JspTagException("Element not found in target page: " + element);
-				} else {
-					targetElement = null;
-				}
-
-				// Write a link to the page
-				final JspWriter out = pageContext.getOut();
-
-				PageIndex pageIndex = PageIndex.getCurrentPageIndex(request);
-				Integer index = pageIndex==null ? null : pageIndex.getPageIndex(targetPageRef);
-
-				out.write("<a");
-				String href;
-				{
-					if(element == null) {
-						// Link to page
-						if(index != null && view == null) {
-							href = '#' + PageIndex.getRefId(index, null);
-						} else {
-							StringBuilder url = new StringBuilder();
-							targetPageRef.appendServletPath(url);
-							if(view != null) {
-								boolean hasQuestion = url.lastIndexOf("?") != -1;
-								url
-									.append(hasQuestion ? "&view=" : "?view=")
-									.append(URLEncoder.encode(view, responseEncoding));
+				JspWriter out = pageContext.getOut();
+				LinkImpl.writeLinkImpl(
+					pageContext.getServletContext(),
+					request,
+					(HttpServletResponse)pageContext.getResponse(),
+					out,
+					book,
+					page,
+					element,
+					view,
+					params,
+					clazz,
+					capturedBody == null || capturedBody.getLength() == 0
+						? null
+						: new LinkImpl.LinkImplBody<JspException>() {
+							@Override
+							public void doBody(boolean discard) throws JspException, IOException, SkipPageException {
+								if(discard) throw new AssertionError("Conditions that lead to discard should have caused no capturedBody above");
+								capturedBody.writeTo(out);
 							}
-							href = url.toString();
 						}
-					} else {
-						if(index != null && view == null) {
-							// Link to target in indexed page (view=all mode)
-							href = '#' + PageIndex.getRefId(index, element);
-						} else if(currentPage!=null && currentPage.equals(targetPage) && view == null) {
-							// Link to target on same page
-							href = '#' + element;
-						} else {
-							// Link to target on different page (or same page, different view)
-							StringBuilder url = new StringBuilder();
-							targetPageRef.appendServletPath(url);
-							if(view != null) {
-								boolean hasQuestion = url.lastIndexOf("?") != -1;
-								url
-									.append(hasQuestion ? "&view=" : "?view=")
-									.append(URLEncoder.encode(view, responseEncoding));
-							}
-							url.append('#').append(element);
-							href = url.toString();
-						}
-					}
-				}
-				UrlUtils.writeHref(out, getJspContext(), href, params, false, LastModifiedServlet.AddLastModifiedWhen.FALSE);
-				if(clazz != null) {
-					if(!clazz.isEmpty()) {
-						out.write(" class=\"");
-						encodeTextInXhtmlAttribute(clazz, out);
-						out.write("\"");
-					}
-				} else {
-					if(targetElement != null) {
-						String linkCssClass = targetElement.getLinkCssClass();
-						if(linkCssClass != null) {
-							out.write(" class=\"");
-							encodeTextInXhtmlAttribute(linkCssClass, out);
-							out.write('"');
-						}
-					}
-				}
-				// No search index all view to avoid duplicate content penalties
-				if("all".equals(view)) {
-					out.write(" rel=\"nofollow\"");
-				}
-				out.write('>');
-
-				if(capturedBody == null || capturedBody.getLength()==0) {
-					if(targetElement != null) {
-						targetElement.appendLabel(new MediaWriter(textInXhtmlEncoder, out));
-					} else if(targetPage!=null) {
-						encodeTextInXhtml(targetPage.getTitle(), out);
-					} else {
-						LinkImpl.writeBrokenPathInXhtml(targetPageRef, element, out);
-					}
-					if(index != null) {
-						out.write("<sup>[");
-						encodeTextInXhtml(Integer.toString(index+1), out);
-						out.write("]</sup>");
-					}
-				} else {
-					capturedBody.writeTo(out);
-				}
-				out.write("</a>");
+				);
 			}
+		} catch(ServletException e) {
+			throw new JspTagException(e);
 		}
 	}
 }
