e8e341048e385bcf21a3b83550108b10a5832737
AO Industries, Inc. <support@aoindustries.com>
2016-08-09 23:18:22 -0500

    Renaming project from ao-web-page-taglib to semanticcms-core-taglib.

--- src/main/java/com/semanticcms/core/taglib/CopyrightTag.java
+++ src/main/java/com/semanticcms/core/taglib/CopyrightTag.java
@@ -1,48 +1,52 @@
 /*
- * ao-web-page-taglib - Java API for modeling web page content and relationships in a JSP environment.
- * Copyright (C) 2015, 2016  AO Industries, Inc.
+ * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
+ * Copyright (C) 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-web-page-taglib.
+ * This file is part of semanticcms-core-taglib.
  *
- * ao-web-page-taglib is free software: you can redistribute it and/or modify
+ * semanticcms-core-taglib is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-web-page-taglib is distributed in the hope that it will be useful,
+ * semanticcms-core-taglib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-web-page-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-core-taglib.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.aoindustries.web.page.taglib;
+package com.semanticcms.core.taglib;
 
+import com.semanticcms.core.model.Copyright;
 import com.semanticcms.core.model.Node;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.servlet.CurrentNode;
-import com.semanticcms.core.servlet.PageRefResolver;
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-public class ChildTag extends SimpleTagSupport {
+public class CopyrightTag extends SimpleTagSupport {
 
-	private String book;
-	public void setBook(String book) {
-		this.book = book;
+	private String rightsHolder;
+	public void setRightsHolder(String rightsHolder) {
+		this.rightsHolder = rightsHolder;
     }
 
-	private String page;
-	public void setPage(String page) {
-		this.page = page;
+	private String rights;
+	public void setRights(String rights) {
+		this.rights = rights;
+    }
+
+	private String dateCopyrighted;
+	public void setDateCopyrighted(String dateCopyrighted) {
+		this.dateCopyrighted = dateCopyrighted;
     }
 
 	@Override
@@ -51,20 +55,9 @@ public class ChildTag extends SimpleTagSupport {
 		final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
 
 		final Node currentNode = CurrentNode.getCurrentNode(request);
-		if(!(currentNode instanceof Page)) throw new JspTagException("<p:child> tag must be nested directly inside a <p:page> tag.");
+		if(!(currentNode instanceof Page)) throw new JspTagException("<p:copyright> tag must be nested directly inside a <p:page> tag.");
 		final Page currentPage = (Page)currentNode;
 
-		try {
-			currentPage.addChildPage(
-				PageRefResolver.getPageRef(
-					pageContext.getServletContext(),
-					request,
-					book,
-					page
-				)
-			);
-		} catch(ServletException e) {
-			throw new JspTagException(e);
-		}
+		currentPage.setCopyright(new Copyright(rightsHolder, rights, dateCopyrighted));
 	}
 }
--- src/main/resources/META-INF/resources/semanticcms-core-taglib/taglibs.inc.jsp
+++ src/main/resources/META-INF/resources/semanticcms-core-taglib/taglibs.inc.jsp
@@ -1,24 +1,24 @@
 <%--
-  ao-web-page-taglib - Java API for modeling web page content and relationships in a JSP environment.
+  semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
   Copyright (C) 2013, 2014, 2016  AO Industries, Inc.
     support@aoindustries.com
     7262 Bull Pen Cir
     Mobile, AL 36695
 
-  This file is part of ao-web-page-taglib.
+  This file is part of semanticcms-core-taglib.
 
-  ao-web-page-taglib is free software: you can redistribute it and/or modify
+  semanticcms-core-taglib is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  ao-web-page-taglib is distributed in the hope that it will be useful,
+  semanticcms-core-taglib is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public License
-  along with ao-web-page-taglib.  If not, see <http://www.gnu.org/licenses/>.
+  along with semanticcms-core-taglib.  If not, see <http://www.gnu.org/licenses/>.
 --%><%@ page language="java" buffer="512kb" autoFlush="true" pageEncoding="UTF-8" session="false"
 %><%-- JSTL 1.1
 --%><%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"
@@ -28,6 +28,6 @@
 --%><%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"
 %><%-- AO Taglib
 --%><%@ taglib prefix="ao" uri="http://aoindustries.com/ao-taglib"
-%><%-- AO Web Page
---%><%@ taglib prefix="p" uri="http://aoindustries.com/ao-web-page-taglib"
+%><%-- SemanticCMS Core
+--%><%@ taglib prefix="p" uri="https://semanticcms.com/semanticcms-core-taglib"
 %>
