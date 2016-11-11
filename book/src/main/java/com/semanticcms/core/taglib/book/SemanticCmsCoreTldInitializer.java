02a938a5bfd00cdabd1765faf304f01b9dddae55
AO Industries, Inc. <support@aoindustries.com>
2016-11-11 16:21:13 -0600

    Documented taglibs.

---
+++ book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
@@ -0,0 +1,57 @@
+/*
+ * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-taglib.
+ *
+ * semanticcms-core-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.taglib.book;
+
+import com.semanticcms.tagreference.TagReferenceInitializer;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class SemanticCmsCoreTldInitializer extends TagReferenceInitializer {
+
+	private static final String TLD_BOOK = "/core/taglib";
+	private static final String TLD_PATH = "/semanticcms-core.tld";
+	static final String TLD_SERVLET_PATH = TLD_BOOK + TLD_PATH;
+
+	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
+	static {
+		additionalApiLinks.put("com.aoindustries.taglib.", "https://aoindustries.com/ao-taglib/apidocs/");
+		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs/");
+		additionalApiLinks.put("com.semanticcms.core.servlet.", "https://semanticcms.com/core/servlet/apidocs/");
+		additionalApiLinks.put("com.semanticcms.core.taglib.", "https://semanticcms.com/core/taglib/apidocs/");
+	}
+
+	public SemanticCmsCoreTldInitializer() {
+		super(
+			"Core Taglib Reference",
+			"Taglib Reference",
+			TLD_BOOK,
+			TLD_PATH,
+			"https://docs.oracle.com/javase/6/docs/api/",
+			"https://docs.oracle.com/javaee/6/api/",
+			additionalApiLinks
+		);
+	}
+}
