a94478befed5e252f16ac77c8f61cda58b8482ae
AO Industries, Inc <support@aoindustries.com>
2019-04-15 05:21:51 -0500

    Now supporting Java 12.
    1) Added new AO Badges.
    
    2) Updated parent POM
    
       a) Changed default Java version from 1.6 to 1.7
    
       b) Renamed Maven build property names to not conflict with newer versions of Java:
          *.jdk15 -> *.jdk5
          *.jdk16 -> *.jdk6
          *.jdk17 -> *.jdk7
          *.jdk18 -> *.jdk8

--- book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
+++ book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
@@ -31,9 +31,6 @@ import com.semanticcms.tagreference.TagReferenceInitializer;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * @author  AO Industries, Inc.
- */
 public class SemanticCmsCoreTldInitializer extends TagReferenceInitializer {
 
 	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
@@ -57,7 +54,7 @@ public class SemanticCmsCoreTldInitializer extends TagReferenceInitializer {
 				),
 				Path.valueOf("/semanticcms-core.tld")
 			),
-			Maven.properties.getProperty("javac.link.javaApi.jdk16"),
+			Maven.properties.getProperty("javac.link.javaApi.jdk6"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			additionalApiLinks
 		);
