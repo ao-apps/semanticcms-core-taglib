bf198177812b07b2dddb2c08f62c2c074ecdfd4f
AO Industries, Inc. <support@aoindustries.com>
2019-10-23 03:36:31 -0500

    Now outputting HTML or XHTML based on the current response content type.
    This should eliminate validator warnings when used as HTML.

---
+++ src/main/java/com/semanticcms/core/taglib/PageTagTEI.java
@@ -0,0 +1,78 @@
+/*
+ * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
+ * Copyright (C) 2019  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-taglib.
+ *
+ * semanticcms-core-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.taglib;
+
+import com.aoindustries.html.Doctype;
+import com.aoindustries.html.Serialization;
+import com.aoindustries.util.MinimalList;
+import java.util.List;
+import java.util.Locale;
+import javax.servlet.jsp.tagext.TagData;
+import javax.servlet.jsp.tagext.TagExtraInfo;
+import javax.servlet.jsp.tagext.ValidationMessage;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class PageTagTEI extends TagExtraInfo {
+
+	@Override
+	public ValidationMessage[] validate(TagData data) {
+		List<ValidationMessage> messages = MinimalList.emptyList();
+		Object serializationAttr = data.getAttribute("serialization");
+		if(
+			serializationAttr != null
+			&& serializationAttr != TagData.REQUEST_TIME_VALUE
+		) {
+			String serialization = ((String)serializationAttr).trim();
+			if(!serialization.isEmpty() && !"auto".equalsIgnoreCase(serialization)) {
+				try {
+					Serialization.valueOf(serialization.toUpperCase(Locale.ROOT));
+				} catch(IllegalArgumentException e) {
+					messages = MinimalList.add(
+						messages,
+						new ValidationMessage(data.getId(), com.aoindustries.taglib.ApplicationResources.accessor.getMessage("HtmlTag.serialization.invalid", serialization))
+					);
+				}
+			}
+		}
+		Object doctypeAttr = data.getAttribute("doctype");
+		if(
+			doctypeAttr != null
+			&& doctypeAttr != TagData.REQUEST_TIME_VALUE
+		) {
+			String doctype = ((String)doctypeAttr).trim();
+			if(!doctype.isEmpty() && !"default".equalsIgnoreCase(doctype)) {
+				try {
+					Doctype.valueOf(doctype.toUpperCase(Locale.ROOT));
+				} catch(IllegalArgumentException e) {
+					messages = MinimalList.add(
+						messages,
+						new ValidationMessage(data.getId(), com.aoindustries.taglib.ApplicationResources.accessor.getMessage("HtmlTag.doctype.invalid", doctype))
+					);
+				}
+			}
+		}
+		return messages.isEmpty() ? null : messages.toArray(new ValidationMessage[messages.size()]);
+	}
+}
