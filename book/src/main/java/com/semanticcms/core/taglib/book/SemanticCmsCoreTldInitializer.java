c9d14a98c952314c933e4f80c844515d63e17abb
AO Industries, Inc <support@aoindustries.com>
2020-03-07 13:19:14 -0600

    Changed default Java version from 1.7 to 1.8 and refactored build properties

--- book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
+++ book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
@@ -54,8 +54,8 @@ public class SemanticCmsCoreTldInitializer extends TagReferenceInitializer {
 				),
 				Path.valueOf("/semanticcms-core.tld")
 			),
-			Maven.properties.getProperty("documented.javac.link.javaApi"),
-			Maven.properties.getProperty("javac.link.javaeeApi.6"),
+			Maven.properties.getProperty("documented.javadoc.link.javase"),
+			Maven.properties.getProperty("javadoc.link.javaee.6"),
 			additionalApiLinks
 		);
 	}
