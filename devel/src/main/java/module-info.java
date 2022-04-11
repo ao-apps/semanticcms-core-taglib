61ad664352174d3d107de5ef5e17e371ba534c83
AO Industries, Inc <support@aoindustries.com>
2022-04-11 12:42:24 -0500

    Marked resource bundles ThreadSafe

--- devel/src/main/java/module-info.java
+++ devel/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -24,6 +24,7 @@ module com.semanticcms.core.taglib.devel {
 	exports com.semanticcms.core.taglib.i18n;
 	// Direct
 	requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
+	requires static jsr305; // <groupId>com.google.code.findbugs</groupId><artifactId>jsr305</artifactId>
 	// Java SE
 	requires java.logging;
 }
