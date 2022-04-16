14f64d7addff536b5df336f1387ef4e4af44bf20
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:28 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
+++ book/src/main/java/com/semanticcms/core/taglib/book/SemanticCmsCoreTldInitializer.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
- * Copyright (C) 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
--- src/main/java/com/semanticcms/core/taglib/WriteNodeBodyTag.java
+++ src/main/java/com/semanticcms/core/taglib/WriteNodeBodyTag.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-taglib - Java API for modeling web page content and relationships in a JSP environment.
- * Copyright (C) 2015, 2016, 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2015, 2016, 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.taglib;
 
 import static com.aoapps.taglib.AttributeUtils.resolveValue;
